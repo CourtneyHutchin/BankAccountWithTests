5ccaece4a5c7c1870a7676115c9814edd5591df4
CourtneyHutchin <courtneyhutchin47@gmail.com>
2020-02-10 14:37:52 -0800

    Added multiple tests for validation

--- BankAccountWithTests.Tests/BankAccountTests.cs
+++ BankAccountWithTests.Tests/BankAccountTests.cs
@@ -11,6 +11,35 @@ namespace BankAccountWithTests.Tests
     [TestClass()]
     public class BankAccountTests
     {
+        [DataRow("123")]
+        [DataRow("A")]
+        [DataRow("1234567890")]
+        [DataRow("ABC123")]
+        [DataRow("ABC123!@#$")]
+        [DataRow("☺")]
+        [TestMethod]
+        public void Constructor_ValidAccNum_SetsAccountNumber(string accNum)
+        {
+            // Arrange / Act
+            BankAccount acc = new BankAccount(accNum);
+
+            // Assert
+            Assert.AreEqual(accNum, acc.AccountNumber);
+        }
+
+        [DataRow("")]
+        [DataRow("   ")]
+        [TestMethod]
+        public void Constructor_InvalidAccNum_ThrowsArgumentException(string accNum)
+        {
+            // Arrange / Act / Assert
+            Assert.ThrowsException<ArgumentException>(() => new BankAccount(accNum));
+
+            // Refactor into separate test
+            BankAccount acc = new BankAccount("123");
+            Assert.ThrowsException<ArgumentException>(() => acc.AccountNumber = accNum);
+        }
+
         [TestMethod()]
         public void Deposit_PositiveValue_AddsToBalance()
         {
@@ -24,6 +53,56 @@ namespace BankAccountWithTests.Tests
 
             // Assert
             Assert.AreEqual(expectedBalance, acc.Balance);
+        }
+
+        [TestMethod]
+        public void Deposit_PositiveAmountWithCents_AddsToBalance()
+        {
+            // Arrange
+            BankAccount acc = new BankAccount("123");
+            double depositAmount = 10.55;
+            double expectedBalance = 10.55;
+
+            // Act
+            acc.Deposit(depositAmount);
+
+            // Assert
+            Assert.AreEqual(expectedBalance, acc.Balance);
+
+        }
+
+        [DataRow(100)]
+        [DataRow(100.99)]
+        [DataRow(.01)]
+        [DataRow(9.000001)]  // We are not losing precision for this one
+        [DataRow(9999999.99)]
+        [TestMethod]
+        public void Deposit_PositiveAmount_ReturnsUpdatedBalance(double depositAmt)
+        {
+            // Arrange
+            BankAccount acc = new BankAccount("321");
+            double initialBalance = 0;
+            double expectedBalance = initialBalance + depositAmt;
+
+            // Act
+            double returnedBalance = acc.Deposit(depositAmt);
+
+            // Assert
+            Assert.AreEqual(expectedBalance, returnedBalance);
+            Assert.AreEqual(expectedBalance, acc.Balance);
+        }
+
+        [DataRow(0)]
+        [DataRow(-.01)]
+        [DataRow(-10000.01)]
+        [TestMethod]
+        public void Deposit_NegativeAmount_ThrowsArgumentException(double depositAmt)
+        {
+            // Arrange
+            BankAccount acc = new BankAccount("123");
+
+            // Assert => Act
+            Assert.ThrowsException<ArgumentException>(() => acc.Deposit(depositAmt));
         }
     }
 }
